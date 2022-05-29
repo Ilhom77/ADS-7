4adb4d6b339e7dc70936c2d5debe730caf78f2c9
Ilkhom Bokhirov <103019100+Ilhom77@users.noreply.github.com>
2022-05-29 15:25:03 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,62 +1,76 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <string>
 template<typename T>
 class TPQueue {
- private:
-  struct ITEM {
-    T value;
-    ITEM* next;
-    ITEM* prev;
-  };
-  TPQueue::ITEM* create(const T& value) {
-    ITEM* item = new ITEM;
-    item->value = value;
-    item->next = item->prev = nullptr;
-    return item;
-  }
-  ITEM* tail;
-  ITEM* head;
-
+  // реализация шаблона очереди с приоритетом на связанном списке
  public:
-  TPQueue() : head(nullptr), tail(nullptr) {}
-  void push(const T& value) {
-    ITEM* itemPlace = head;
-    ITEM* item = create(value);
-    while (itemPlace && itemPlace->value.prior >= value.prior)
-      itemPlace = itemPlace->next;
-    if (!itemPlace && head) {
-      tail->next = item;
-      item->prev = tail;
-      tail = item;
-      return;
-    } else if (!itemPlace && !head) {
-      head = tail = item;
-    } else if (!itemPlace->prev) {
-      head->prev = item;
-      item->next = head;
-      head = item;
-    } else {
-      itemPlace->prev->next = item;
-      item->prev = itemPlace->prev;
-      item->next = itemPlace;
-      itemPlace->prev = item;
-    }
-  }
-  T pop() {
-    if (!head || !tail) throw "The queue is empty";
-    ITEM* temp = head->next;
-    if (temp) temp->prev = nullptr;
-    T value = head->value;
-    delete head;
-    head = temp;
-    if (!head) tail = nullptr;
-    return value;
-  }
+      TPQueue(): head(nullptr), tail(nullptr) {}
+      void push(const T&);
+      T pop();
+ private:
+      struct ITEM {
+          T value;
+          ITEM *next;
+          ITEM *prew;
+      };
+      TPQueue:: ITEM *create(const T&);
+      ITEM *head;
+      ITEM *tail;
 };
-
 struct SYM {
   char ch;
   int prior;
 };
-#endif  // INCLUDE_TPQUEUE_H
+template<typename T>
+typename TPQueue<T>::ITEM *TPQueue<T>::create(const T& value) {
+  ITEM * temp = new ITEM;
+  temp->value = value;
+  temp->next = nullptr;
+  temp->prew = nullptr;
+  return temp;
+}
+template<typename T>
+void TPQueue <T>::push(const T& data) {
+  ITEM *temp = head;
+  ITEM* item = create(data);
+  while (temp && temp->value.prior >= data.prior) {
+    temp = temp->next;
+  }
+  if (!temp && head) {
+    tail ->next = item;
+    item->prew = tail;
+    tail = item;
+  } else if (!temp && !head) {
+    head = tail = item;
+  } else if (!temp->prew) {
+    head->prew = item;
+    item->next = head;
+    head = item;
+  } else {
+    temp->prew->next = item;
+    item->prew = temp->prew;
+    item->next = temp;
+    temp->prew = item;
+  }
+}
+template<typename T>
+T TPQueue<T>::pop() {
+  if (head && tail) {
+    ITEM* temp = head->next;
+    if (temp) {
+      temp->prew = nullptr;
+    }
+    T data = head->value;
+    delete head;
+    head = temp;
+    if (!head) {
+      tail = nullptr;
+    }
+    return data;
+  } else {
+    throw std::string("Is Empty!");
+  }
+}
+#endif  // INCLUDE_TPQUEUE_H_
